06a11ea95ec10fe2526b157baafeedcff629a9e8
Ariel Leventhal <arielleventhal@gmail.com>
2024-09-05 18:16:44 -0300

    initial

---
+++ software/niosLab2_bsp/obj/HAL/src/crt0.d
@@ -0,0 +1,17 @@
+obj/HAL/src/crt0.o: HAL/src/crt0.S system.h linker.h HAL/inc/nios2.h \
+ HAL/inc/sys/alt_log_printf.h drivers/inc/altera_avalon_jtag_uart_regs.h \
+ HAL/inc/io.h HAL/inc/alt_types.h
+
+system.h:
+
+linker.h:
+
+HAL/inc/nios2.h:
+
+HAL/inc/sys/alt_log_printf.h:
+
+drivers/inc/altera_avalon_jtag_uart_regs.h:
+
+HAL/inc/io.h:
+
+HAL/inc/alt_types.h:
